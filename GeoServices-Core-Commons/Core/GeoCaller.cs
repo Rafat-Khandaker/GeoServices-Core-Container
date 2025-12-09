c7a201bdf2266a04b85cc90d96037db165f6f8a8
Rafat Khandaker <RKhandaker@planning.nyc.gov>
2025-12-09 16:45:18 -0500

    Add Mutex SafeHandle Option for locking Geo.dll resource

--- GeoServices-Core-Commons/Core/GeoCaller.cs
+++ GeoServices-Core-Commons/Core/GeoCaller.cs
@@ -13,96 +13,102 @@ namespace GeoServices_Core_Commons.Core
     public class GeoCaller : IGeoCaller
     {
         public Geo Geo { get; set; }
-
-        public delegate void GeoCallFunction();
+        private Mutex Mutex { get; set; }
+        private delegate void GeoCallFunction();
 
         public GeoCaller(Geo _geo)
         {
             Geo = _geo;
+            Mutex = new Mutex();
         }
 
-        private async Task SafeHanddle(GeoCallFunction geoCallfunction, int retry = 1)
+        private async Task SafeHanddle(GeoCallFunction geoCallfunction, bool lockResource = false, int retry = 1 )
         {
             try {
-                await Task.Run(()=> geoCallfunction());
+                if (lockResource) {
+                    if (Mutex.WaitOne(10)) {
+                        await SafeHanddle(geoCallfunction, lockResource, retry++);
+                        Mutex.ReleaseMutex();
+                    }
+                }
+                else await Task.Run(() => geoCallfunction());
             }
             catch {
 
                 if (retry < 3)
                 {
                     Thread.Sleep(100);
-                    await SafeHanddle(geoCallfunction, retry++);
                 }
                 else return;
             }
         }
 
-        public async Task GeoCall(WA inwa1) => await SafeHanddle(() => Geo.GeoCall(ref inwa1));
+        public async Task GeoCall(WA inwa1, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1), lockResource);
             
 
-        public async Task GeoCall(Wa1 inwa1) => await SafeHanddle(() => Geo.GeoCall(ref inwa1));
+        public async Task GeoCall(Wa1 inwa1, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2F1 inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
-        public async Task GeoCall(Wa1 inwa1, Wa2F1ex inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2F1 inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
+        public async Task GeoCall(Wa1 inwa1, Wa2F1ex inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2F1a inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2F1a inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2F1ax inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2F1ax inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2F1al inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2F1al inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2F1al_TPAD inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2F1al_TPAD inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2F1e inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2F1e inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2F1v inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2F1v inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2F1b inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2F1b inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2F2 inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2F2 inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2F2w inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2F2w inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2F3 inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2F3 inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2F3as inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2F3as inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2F3c inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2F3c inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2F3cas inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2F3cas inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2F3x inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2F3x inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2F3xas inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2F3xas inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2F3cx inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2F3cx inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2F3cxas inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2F3cxas inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2F3e inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2F3e inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2F3eas inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2F3eas inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2F3ce inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2F3ce inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2F3ceas inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2F3ceas inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2F3s inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2F3s inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2F5 inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2F5 inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2F1p inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2F1p inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2Fap inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2Fap inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2Fapx inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2Fapx inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(Wa1 inwa1, Wa2Fhr inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(Wa1 inwa1, Wa2Fhr inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task GeoCall(WA inwa1, WA inwa2) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2));
+        public async Task GeoCall(WA inwa1, WA inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1, ref inwa2), lockResource);
 
-        public async Task NYCcall(WA inwa1) => await SafeHanddle(() => Geo.GeoCall(ref inwa1));
+        public async Task NYCcall(WA inwa1, bool lockResource = false) => await SafeHanddle(() => Geo.GeoCall(ref inwa1), lockResource);
 
-        public async Task NYCcall(WA inwa1, WA inwa2) => await SafeHanddle(() => Geo.NYCcall(ref inwa1, ref inwa2));
+        public async Task NYCcall(WA inwa1, WA inwa2, bool lockResource = false) => await SafeHanddle(() => Geo.NYCcall(ref inwa1, ref inwa2), lockResource);
     }
 }
